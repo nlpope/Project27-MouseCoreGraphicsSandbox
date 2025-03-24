7e91552ddfaeddcbca6ec5a3648192fc355cb77c
Noah Pope <npope@tutanota.com>
2025-03-24 12:07:45 -0500

    initial commit

---
+++ Project27-MouseCoreGraphicsSandbox/Screens/HomeVC.swift
@@ -0,0 +1,21 @@
+//  File: HomeVC.swift
+//  Project: Project27-MouseCoreGraphicsSandbox
+//  Created by: Noah Pope on 3/24/25.
+
+import UIKit
+
+class HomeVC: UIViewController
+{
+    @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func redrawTapped(_ sender: Any)
+    {
+        
+    }
+}
