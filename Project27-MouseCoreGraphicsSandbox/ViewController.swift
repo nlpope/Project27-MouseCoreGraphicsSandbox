c9e1cd8f0edbd4c81ad7fc3b870faa88148d5465
Noah Pope <npope@tutanota.com>
2025-03-24 11:57:32 -0500

    Initial Commit

---
+++ Project27-MouseCoreGraphicsSandbox/ViewController.swift
@@ -0,0 +1,16 @@
+//  File: ViewController.swift
+//  Project: Project27-MouseCoreGraphicsSandbox
+//  Created by: Noah Pope on 3/24/25.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Project27-MouseCoreGraphicsSandboxTests/Project27_MouseCoreGraphicsSandboxTests.swift
@@ -0,0 +1,14 @@
+//  File: Project27_MouseCoreGraphicsSandboxTests.swift
+//  Project: Project27-MouseCoreGraphicsSandboxTests
+//  Created by: Noah Pope on 3/24/25.
+
+import Testing
+@testable import Project27_MouseCoreGraphicsSandbox
+
+struct Project27_MouseCoreGraphicsSandboxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
